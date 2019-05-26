a66390c299f8eff444d63f9c3f05eb72582c1581
Flooorent <florent.moiny@gmail.com>
2019-05-26 15:10:04 +0200

    use react-fontawesome, handle reset, incremend and decrement clicks

--- src/App.js
+++ src/App.js
@@ -7,16 +7,25 @@ import LengthPicker from './LengthPicker'
 class App extends React.Component {
   constructor(props) {
     super(props)
-  }
 
-  componentWillMount() {
-    this.fontAwesomeLink = document.createElement('link')
-    this.fontAwesomeLink.rel = 'stylesheet'
-    this.fontAwesomeLink.href = 'https://use.fontawesome.com/releases/v5.8.2/css/all.css'
-    this.fontAwesomeLink.integrity = 'sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay'
-    this.fontAwesomeLink.crossOrigin = 'anonymous'
+    this.defaultBreakLength = 5
+    this.defaultSessionLength = 25
+    this.minLength = 1
+    this.maxLength = 60
 
-    document.body.appendChild(this.fontAwesomeLink)
+    this.state = {
+      lengths: {
+        break: this.defaultBreakLength,
+        session: this.defaultSessionLength
+      },
+      timeLeft: this.defaultSessionLength,
+      currentTimer: 'Session',
+      isRunning: false
+    }
+
+    this.handleReset = this.handleReset.bind(this)
+    this.handleDecrementClick = this.handleDecrementClick.bind(this)
+    this.handleIncrementClick = this.handleIncrementClick.bind(this)
   }
 
   componentDidMount() {
@@ -34,23 +43,95 @@ class App extends React.Component {
     document.body.appendChild(script)
   }
 
+  handleReset() {
+    this.setState({
+      lengths: {
+        break: this.defaultBreakLength,
+        session: this.defaultSessionLength
+      },
+      timeLeft: this.defaultSessionLength,
+      currentTimer: 'Session',
+      isRunning: false
+    })
+  }
+
+  handleDecrementClick(breakOrSession) {
+    if(!this.state.isRunning) {
+      const currentLength = this.state.lengths[breakOrSession]
+      
+      if(currentLength > this.minLength) {
+        const newLengths = {...this.state.lengths}
+        const newBreakOrSessionLength = currentLength - 1
+        newLengths[breakOrSession] = newBreakOrSessionLength
+
+        const newState = {
+          lengths: newLengths 
+        }
+
+        if(breakOrSession.toLowerCase() === "session") {
+          newState.timeLeft = newBreakOrSessionLength
+        }
+
+        this.setState(newState)
+      }
+    }
+  }
+
+  handleIncrementClick(breakOrSession) {
+    if(!this.state.isRunning) {
+      const currentLength = this.state.lengths[breakOrSession]
+      
+      if(currentLength < this.maxLength) {
+        const newLengths = {...this.state.lengths}
+        const newBreakOrSessionLength = currentLength + 1
+        newLengths[breakOrSession] = newBreakOrSessionLength
+
+        const newState = {
+          lengths: newLengths 
+        }
+
+        if(breakOrSession.toLowerCase() === "session") {
+          newState.timeLeft = newBreakOrSessionLength
+        }
+
+        this.setState(newState)
+      }
+    }
+  }
+
   render() {
+    const formattedTimeLeft = this.state.timeLeft.toString().padStart(2, '0') + ':00'
+
     return (
       <div className="App">
         <div id="pomodoro-container">
+
           <header>Pomodoro Clock</header>
+
           <div id="lengths-picker-container">
             <LengthPicker
-              name="Break Length"
               label="break"
+              length={this.state.lengths.break}
+              handleDecrementClick={() => this.handleDecrementClick("break")}
+              handleIncrementClick={() => this.handleIncrementClick("break")}
             />
             <LengthPicker
-              name="Session Length"
               label="session"
+              length={this.state.lengths.session}
+              handleDecrementClick={() => this.handleDecrementClick("session")}
+              handleIncrementClick={() => this.handleIncrementClick("session")}
             />
           </div>
-          <div id="session"></div>
-          <div id="controls"></div>
+
+          <div id="session">
+            <div id="timer-label">{this.state.currentTimer}</div>
+            <div id="time-left">{formattedTimeLeft}</div>
+          </div>
+
+          <div id="controls">
+            <div id="start_stop"></div>
+            <button id="reset" type="button" onClick={this.handleReset}>Reset</button>
+          </div>
         </div>
       </div>
     );
--- src/LengthPicker.js
+++ src/LengthPicker.js
@@ -1,17 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import './LengthPicker.css'
 
 function LengthPicker(props) {
     return (
         <div className="length-picker-container">
-            <div id={`${props.label}-label`}>{props.name}</div>
+            <div id={`${props.label}-label`}>{props.label} length</div>
             <div className="picker-container">
-                <div id={`${props.label}-decrement`}></div>
-                <div>{props.length}</div>
-                <div id={`${props.label}-increment`}></div>
+                <FontAwesomeIcon
+                    id={`${props.label}-decrement`}
+                    icon={faArrowDown}
+                    onClick={props.handleDecrementClick}
+                />
+
+                <div id={`${props.label}-length`}>{props.length}</div>
+
+                <FontAwesomeIcon
+                    id={`${props.label}-increment`}
+                    icon={faArrowUp}
+                    onClick={props.handleIncrementClick}
+                />
             </div>
         </div>
     )
+}
+
+LengthPicker.propTypes = {
+    label: PropTypes.string.isRequired,
+    length: PropTypes.number.isRequired,
+    handleDecrementClick: PropTypes.func.isRequired,
+    handleIncrementClick: PropTypes.func.isRequired,
 }
 
 export default LengthPicker

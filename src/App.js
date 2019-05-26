d1901cd412b75fe419889c5d40a4b26dbb2d43a5
Flooorent <florent.moiny@gmail.com>
2019-05-26 10:43:01 +0200

    add fcc tester

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.addFCCTester()
+  }
+
+  addFCCTester() {
+    const script = document.createElement('script')
+    script.src = 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js'
+    script.async = true
+    document.body.appendChild(script)
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
